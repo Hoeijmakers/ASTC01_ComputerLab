92c95f8b1a9f077b31d42ad2937d1f9dc730e175
bprinoth <34542496+bprinoth@users.noreply.github.com>
2020-10-23 09:46:50 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 import TransitSimulator
 
-sys1 = System('WASP-121b', 1.183, 1.865, 3.34, 0.0558, 1.45, 1.5, 0,  np.pi / 2., 0)
+sys1 = TransitSimulator.System('WASP-121b', 1.183, 1.865, 3.34, 0.0558, 1.45, 1.5, 0,  np.pi / 2., 0)
 
 sys1.TransitParams()
 sys1.Transit()
